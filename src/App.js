ee1522e4e3b317ed3ed3906af4c33fdaed531978
silviosoto <silviojsoto@gmail.com>
2024-03-23 22:57:56 -0500

    interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './css/styles.css'
+import Register from "./component/register";
+import Home from "./Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+ 
+           <Route index path="/" element={<Home />} />
+            <Route path="/register" element={<Register />} />
+            {/* <Route path="*" element={<App />} /> */}
+        </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/component/register.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+// import './App.css';
+import '../css/styles.css'
+
+function Register() {
+  return (
+   <h1>register</h1>
+  );
+}
+
+export default Register;
